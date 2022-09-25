776f6933741adaac7748e79e9b8d7fcf95b70692
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-09-25 12:54:33 -0400

    Add server socker code and remove flask

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask, request
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-   print("Hello World")
-   return request.args.get('text', '')
+from server import run_server
 
 if __name__ == '__main__':
-   app.run("0.0.0.0", "80")
+   run_server("0.0.0.0", 80)
---
+++ server.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+from threading import Thread
+
+"""
+Function to start Server
+
+Parameters:
+    ip_addr (str): IP Address
+    port (int): Port Number
+
+Returns:
+
+"""
+def run_server(ip_addr, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip_addr, port))
+    wait_for_connections(server_socket)
+
+def wait_for_connections(server_socket):
+    server_socket.listen(3)
+    conn, addr = server_socket.accept()
+    Thread(target=start_client, args=(conn, addr)).start()
+
+
+def start_client(conn, addr):
+    while True:
+        try:
+            data = conn.recv(1024)  # receive data from client
+        except Exception as e:
+            print("connection closed" + str(e))
+            sys.exit()
+        if len(data) > 0:
+            client_message = bytes.decode(data)  # decode it to string
+            return client_message
+        
