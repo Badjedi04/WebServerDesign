60d61587fd2bbf48a63b3d1a2294cbcfc8a77872
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-02 20:34:20 -0400

    Add Server Reply

--- parser.py
+++ parser.py
@@ -4,5 +4,29 @@
 
     Returns:
 """
+import json
+import os
+import constants
+import report.response as response
+
+
 def get_request_header(request_header):
     print(request_header)
+    dict_request = {}
+    for index, line in enumerate(request_header.splitlines()):
+        if index > 0:
+            line_splitter = line.split(":")
+            dict_request[line_splitter[0]] = line_splitter[1].strip()
+        else:
+            line_splitter = line.split()
+            dict_request["method"] = line_splitter[0]
+            dict_request["path"] = line_splitter[1]
+            dict_request["http_version"] = line_splitter[2]
+    with open(os.path.join(constants.DATA_DIR, "request.json"), "w") as fobj:
+        json.dump(dict_request, fobj)
+    print(dict_request)
+    return(response.create_server_response(dict_request))
+
+
+
+    
