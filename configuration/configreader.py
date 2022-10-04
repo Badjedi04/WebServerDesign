0e251c711d7709997bc38f4344e6e5285abb8956
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-04 19:08:10 -0400

    Add another dict for configread

--- configuration/configreader.py
+++ configuration/configreader.py
@@ -4,14 +4,17 @@ import constants
 
 def read_config_file():
     try:
+        dict_config = {}
         config = configparser.ConfigParser()
         config.read(constants.CONFIG)
+
         for section in config.sections():
             sys.stdout.write(f'section: {section}\n')
+            dict_config[section] = {}
             for (key, value) in config[section].items():
                 sys.stdout.write(f'key: {key}  value: {value}\n')
-                config[section][key] = convert_to_int(value)
-        return config
+                dict_config[section][key] = convert_to_int(value)
+        return dict_config
     except Exception as e:
         sys.stderr.write(f'read_config_file: error: {e}\n')
 def convert_to_int(value):
