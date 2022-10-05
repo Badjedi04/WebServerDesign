2e72165089d194c255ff7b48a7f93571b249e3b7
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-04 20:59:58 -0400

    Fix dict request not present in server reply

--- report/reply_header.py
+++ report/reply_header.py
@@ -9,31 +9,32 @@ def create_response_header(status_code, config, dict_request=None):
         dict_response["status_code"] = status_code
         dict_response["http_version"] = config["HEADERS"]["http_version"]
         dict_response["status_text"] = config["STATUS_CODE"][status_code]
-        response = return_mime_type(config, dict_request["path"])
-        dict_response["Content-type"] = f'{response["mime_type"]}; charset=iso-8859-1'
         dict_response["Server"] = config["HEADERS"]["server"]
-        if "length" in response:
-            dict_response["Content-Length"] = response["length"]
-        if "payload" in response and dict_request["method"] == "GET":
-            dict_response["payload"] = response["payload"]
-        if dict_request["Connection"]:
-            dict_response["Connection"] = dict_request["Connection"]
+        if dict_request:
+            response = return_mime_type(config, dict_request["path"])
+            dict_response["Content-type"] = f'{response["mime_type"]}; charset=iso-8859-1'
+            if "length" in response:
+                dict_response["Content-Length"] = response["length"]
+            if "payload" in response and dict_request["method"] == "GET":
+                dict_response["payload"] = response["payload"]
+            if dict_request["Connection"]:
+                dict_response["Connection"] = dict_request["Connection"]
 
 
         with open(constants.RESPONSE_REPORT, "w") as fobj:
             json.dump(dict_response,fobj)
     except Exception as e:
-        sys.stderr.write(f'create_response: error {e}')
+        sys.stderr.write(f'create_response_header: error {e}\n')
 
-def return_mime_type(config, file=None):
+def return_mime_type(config, file_path=None):
     try:
-        if file is None:
+        if file_path is None:
             return {"mime_type": config["HEADERS"]["mime_types"][1]}
         else:
-            with open(file, "rb") as fobj:
+            with open(file_path, "rb") as fobj:
                 file_response = fobj.read()
                 file_length = len(file_response)
-            file_ext = file.split("/")[-1].split(".")[-1]
+            file_ext = file_path.split("/")[-1].split(".")[-1]
             if file_ext == "html":
                 return {"mime_type": config["HEADERS"]["mime_types"][1], "length": file_length, "payload": file_response}
             elif file_ext == "xml":
@@ -51,4 +52,4 @@ def return_mime_type(config, file=None):
             elif file_ext in ["docx", "doc"]:
                 return {"mime_type": config["HEADERS"]["mime_types"][8], "length": file_length, "payload": file_response}
     except Exception as e:
-        sys.stderr.write(f'return_mime_type: error {e}')
+        sys.stderr.write(f'return_mime_type: error {e}\n')
