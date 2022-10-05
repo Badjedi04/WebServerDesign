84486349f6e972a834d097ab8190755f76dd57b9
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-04 20:54:34 -0400

    Add try catch in responder

--- report/responder.py
+++ report/responder.py
@@ -1,15 +1,20 @@
 import json
 import os
+import sys
 
 import constants
 import report.reply_header as reply_header
 
 def handle_server_request(config):
-    with open(constants.REQUEST_REPORT , "r") as fobj:
-        dict_request = json.load(fobj) 
-    if dict_request["method"] in ["GET", "HEAD"]:
-        dict_request["path"] = dict_request["path"].replace(config["MAPPING"]["host_path"], config["MAPPING"]["root_dir"])
-        if os.path.exists(dict_request["path"]):
-            reply_header.create_response_header("200", config, dict_request)
-        else:
-            reply_header.create_response_header("404", config, dict_request)
+    try:
+        with open(constants.REQUEST_REPORT , "r") as fobj:
+            dict_request = json.load(fobj) 
+        if dict_request["method"] in ["GET", "HEAD"]:
+            dict_request["path"] = dict_request["path"].replace(config["MAPPING"]["host_path"], config["MAPPING"]["root_dir"])
+            sys.stdout.write(f'handle_server_request: path: {dict_request["path"]}\n')
+            if os.path.exists(dict_request["path"]):
+                reply_header.create_response_header("200", config, dict_request)
+            else:
+                reply_header.create_response_header("404", config, dict_request)
+    except Exception as e:
+        sys.stderr.write(f'handle_server_request: error: {e}\n')
