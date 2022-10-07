8d89998ba343606181d0646d1ef60f5a76024402
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-07 00:25:37 -0400

    Add logs in clodse connection

--- server.py
+++ server.py
@@ -18,28 +18,34 @@ def run_server(config):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((config["SERVER"]["ip_addr"], config["SERVER"]["port"]  ))
-    wait_for_connections(server_socket, config)
+    connection_timeout = None
 
-def wait_for_connections(server_socket, config):
+    wait_for_connections(server_socket, config, connection_timeout)
+
+def wait_for_connections(server_socket, config, connection_timeout):
     while True:
         server_socket.listen(config["SERVER"]["connections"])
         conn, addr = server_socket.accept()
-        Thread(target=start_client, args=(conn, addr, config)).start()
+        Thread(target=start_client, args=(conn, addr, config, connection_timeout)).start()
 
 
 def close_connection(conn):
+    sys.stdout.write("Going to close connection\n")
+    sys.stdout.write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
     conn.shutdown(socket.SHUT_RDWR)
     conn.close()
 
 
-def start_client(conn, addr, config):
+def start_client(conn, addr, config, connection_timeout):
     while True:
         try:
             data = conn.recv(1024)  # receive data from client
-            connection_timeout = Timer(30, close_connection, args=(conn))
-            connection_timeout.start()
-            if data:
 
+            if data:
+                if connection_timeout is not None:
+                    connection_timeout.cancel()
+                connection_timeout = Timer(15, close_connection, args=(conn))
+                connection_timeout.start()                
                 sys.stdout.write("*********************************************************************************\n")
                 sys.stdout.write("Server Data received\n")
                 response = parser.get_request_header(data.decode(), config)
