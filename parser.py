0bd786e923275092192a5292f23594423ee9770e
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-07 01:01:01 -0400

    Fix parser code

--- parser.py
+++ parser.py
@@ -12,7 +12,11 @@ import report.reply_header as reply_header
 def get_request_header(request_header, config):
     try:
         sys.stdout.write(f'Print request_header: \n{request_header}\n')
-        return header_validate(request_header, config)
+        report =  header_validate(request_header, config)
+        if "response" in report:
+            return report
+        else:
+            parse_header(request_header)
     except Exception as e:
         sys.stderr.write(f'Parser: get_request_header error: {e}\n')
 
@@ -25,43 +29,42 @@ def header_validate(request_header, config):
 
     """
     try:
+        report = {"response": {}}
         line_splitter = request_header.splitlines()
-        dict_request = parse_header(line_splitter)
         sys.stdout.write(f'Request Header size: {len(line_splitter)}\n')
         sys.stdout.write(f'Request Header : {line_splitter}\n')
-        if line_splitter[-1].strip():
-            sys.stdout.write("Line is empty found**************\n")
-        if line_splitter[-1] in ['\n', '\r\n']:
-            sys.stdout.write("Line is empty found case 2**************\n")
         if len(line_splitter[-1]) > 0:
             sys.stdout.write("Last line is not empty\n")
-            return reply_header.create_response_header("400", config, dict_request)
+            report["response"]["status_code"] = "400"
         for index, line in enumerate(line_splitter[:-1]):
             sys.stdout.write(f'validate header: {line}\n')
             if index == 0:
                 line_splitter = line.split()
                 if len(line_splitter) != 3:
                     sys.stdout.write("First line is more than three\n")
-                    return reply_header.create_response_header("400", config, dict_request)
+                    report["response"]["status_code"] = "400"
+
                 elif line_splitter[0] not in config["HEADERS"]["http_methods"]:
                     sys.stdout.write("HTTP method not supported\n")
-                    return reply_header.create_response_header("501", config, dict_request)
+                    report["response"]["status_code"] = "501"
+
                 elif line_splitter[2]: 
                     version_splitter = line_splitter[2].split("/")
                     if version_splitter[0] != "HTTP":
                         sys.stdout.write("HTTP not used \n")
-                        return reply_header.create_response_header("400", config, dict_request)
+                        report["response"]["status_code"] = "400"
                     elif version_splitter[1] != config["HEADERS"]["http_version"]:
                         sys.stdout.write("HTTP version is wrong \n")
-                        return reply_header.create_response_header("505", config, dict_request)
+                        report["response"]["status_code"] = "505"
             else:
                 line_splitter = line.split(":")
                 if len(line_splitter) != 2 \
                     or line_splitter[0] == "Host" and line_splitter[1].strip() != "cs531-cs_ptoma001"\
                     or line_splitter[0]  == "Connection" and line_splitter[1].strip() != "close":
                     sys.stdout.write("Either not key value pair, host name or connection is wrong \n")
-                    return reply_header.create_response_header("400", config, dict_request)
-        return dict_request   
+                    report["response"]["status_code"] = "400"
+
+        return report   
     except Exception as e:
         sys.stderr.write(f'header_validate: error {e}\n')
 
