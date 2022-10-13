47ee73249adcd77464d66d80ecef03b4744871d6
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-12 22:02:39 -0400

    Push assignment1

--- constants.py
+++ constants.py
@@ -5,3 +5,4 @@ CONFIG = os.path.join(CONFIG_DIR, "config.ini")
 DATA_DIR = "data"
 REQUEST_REPORT = os.path.join(DATA_DIR, "request_header.json")
 RESPONSE_REPORT = os.path.join(DATA_DIR, "response_header.json")
+
