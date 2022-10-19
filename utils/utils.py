11c27e1ac7f2a9da0ccc05b9cbba835e5f548ad4
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-10-18 23:38:09 -0400

    fix timestamp1

--- utils/utils.py
+++ utils/utils.py
@@ -13,7 +13,7 @@ def get_file_last_modified_time(file_path):
     try:
         statinfo = os.stat(file_path)
         last_modified = datetime.utcfromtimestamp(statinfo.st_mtime)
-        return convert_timestamp_to_gmt(last_modified)
+        return last_modified.strftime("%a, %d %b %Y %H:%M:%S GMT")
     except Exception as e:
         sys.stderr.write(f'get_file_last_modified_time: error: {e}')
         return None     
