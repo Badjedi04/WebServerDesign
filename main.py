4f89343effbae2a00b8a8dec3ee41c2a01cd575d
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-12-04 14:05:32 -0500

    Remove files

--- main.py
+++
@@ -1,8 +0,0 @@
-from server import run_server
-import configuration.configwriter as configwriter
-import configuration.configreader as configreader
-
-if __name__ == '__main__':
-   configwriter.create_config_file()
-   config = configreader.read_config_file()   
-   run_server(config)
