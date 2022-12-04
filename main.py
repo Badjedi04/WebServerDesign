fd14458165799c14ac0b925050c86fe5269e2a94
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-12-04 13:45:55 -0500

    Remove useless files

--- main.py
+++
@@ -1,8 +0,0 @@
-from echoserver import start_server
-from Configuration.configwriter import write_config
-from Configuration.configreader import config_file_reader
-
-if __name__ == '__main__':
-     write_config()
-     config = config_file_reader()
-     start_server(config)
