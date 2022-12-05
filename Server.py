d9d891bb1a9558bfc226f1c317e16afb20e6750b
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-12-04 19:51:28 -0500

    Add config files6

--- Server.py
+++ Server.py
@@ -392,10 +392,8 @@ class Server:
         if request_method == "GET" or request_method == "HEAD":
             self.debug_logger.debug("fetch_resource: request method: "+ request_method)
             requested_resource = unquote(requested_resource)
-            for key in self.config_access.virtual_uri:
-                if key in requested_resource:
-                    requested_resource = "access.log"
-                    break
+            if self.config_access.logs_redirect in requested_resource:
+                requested_resource = "access.log"
             if not os.path.exists(self.config_instance.root_folder + requested_resource):
                 self.debug_logger.debug(str(self.config_instance.root_folder + requested_resource))
                 self.debug_logger.debug("Requested resource missing 404: " + requested_resource)
