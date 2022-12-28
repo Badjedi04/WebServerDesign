fc866a0f38ccc97169f97e7ee34550708a7e8726
Prashant Tomar <prashant.tomar@rocketmail.com>
2022-12-28 10:56:28 -0500

    Delete files4

--- src/AuthorizationStructure.py
+++
@@ -1,8 +0,0 @@
-class AuthorizationStructure:
-    def __init__(self):
-        self.report = \
-            {
-                "authorization_type": None,
-                "realm": None,
-                "users": None
-            }
