3cf62bc5562b07e6c8c2e451f076162170a81982
Prashant Tomar <prashant.tomar@rocketmail.com>
2023-01-05 12:26:12 -0500

    update server.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from server import run_server
 import Config.configwriter as configwriter
 import Config.configreader as configreader
 
 if __name__ == '__main__':
    configwriter.create_config_file()
    config = configreader.read_config_file()   
-   run_server(config)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import sys
 from threading import Thread, Timer
 
 import parser_request as parser
-import server_report.responder as responder
+import server_report as responder
 
 """
 Function to start Server
@@ -25,6 +25,10 @@ def wait_for_connections(server_socket, config):
 
 
 def close_connection(conn, timeout=False, config=None):
+    sys.stdout.write("Going to close connection\n")
+    sys.stdout.write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+    sys.stdout.write("Going to close connection\n")
+    sys.stdout.write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
     if timeout:
         report = {}
         report["response"] = {}
@@ -35,6 +39,8 @@ def close_connection(conn, timeout=False, config=None):
         report["response"]["Connection"] = "close" 
         
         conn.send(responder.server_reply(config, report))
+    sys.stdout.write("Going to close connection\n")
+    sys.stdout.write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
     if timeout:
         report = {}
         report["response"] = {}
@@ -52,30 +58,41 @@ def close_connection(conn, timeout=False, config=None):
 def start_client(conn, addr, config):
     connection_timeout = None
     while True:
+        try:
             data = conn.recv(1024)  # receive data from client
             if data:
                 if connection_timeout is not None:
                     connection_timeout.cancel()
                 connection_timeout = Timer(config["SERVER"]["timeout"], close_connection, args=(conn, True, config))
                 connection_timeout.start()                
+                sys.stdout.write("*********************************************************************************\n")
+                sys.stdout.write("Server Data received\n")
                 server_report_header = data.decode()
                 response_header = decompose_headers(response_header, config)
                 for header in response_header:
                     server_report = parser.get_request_header(header, config)
+                    sys.stdout.write("Server Data Parsed\n")
                     server_response = responder.handle_server_response(config, server_report)
                     if server_response:
                         conn.send(server_response)
                         if "Connection" in server_report["request"] and server_report["request"]["Connection"] == "close":
+                            sys.stdout.write("Connection close called due to Connection:close header\n")
                             connection_timeout.cancel()
                             close_connection(conn)
                     else:
                         conn.send(str.encode("null"))
+                sys.stdout.write("Server response sent\n")
+                sys.stdout.write("???????????????????????????????????????????????????????????????????????????????\n")
                 break
+        except Exception as e:
+            sys.stderr.write(f'start_client:error: {e}\n')
 
 
 def decompose_headers(response_header, config):
+    sys.stdout.write(f'decompose_headers called\n')
     list_header_splitter = response_header.splitlines()
     list_header = []
+    sys.stdout.write(f'Header splitted: \n {list_header_splitter}\n')
     temp = ""
     for line in list_header_splitter:
         if len(line) > 0:
@@ -84,4 +101,5 @@ def decompose_headers(response_header, config):
             temp += "\n"
             list_header.append(temp)
             temp = ""
+        sys.stdout.write(f'Header splitted each: \n {list_header}\n')
     return list_header
