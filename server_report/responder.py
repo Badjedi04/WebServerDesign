59698c77a65bf0ffc69912999c58b948f9fda774
Prashant Tomar <prashant.tomar@rocketmail.com>
2023-03-10 20:26:11 -0500

    Update responder.py

--- server_report/responder.py
+++ server_report/responder.py
@@ -16,21 +16,14 @@ def handle_server_request(config, report):
             sys.stdout.write(f'handle_server_request: path: {report["request"]["path"]}\n')
             if os.path.exists(report["request"]["path"]):
                 report["response"]["status_code"] = "200"
-                with open(report["request"]["path"]) as f:
-                    report["response"]["body"] = f.read()
                 sys.stdout.write(f'handle_server_request: 200 \n')
                 return reply_header.create_response_header(config, report)
             else:
                 sys.stdout.write(f'handle_server_request: 404 \n')
                 report["response"]["status_code"] = "404"
-                report["response"]["body"] = "404 Not Found"
                 return reply_header.create_response_header(config, report)
         elif report["request"]["method"] in ["OPTIONS", "TRACE"]:   
             report["response"]["status_code"] = "200" 
-            report["response"]["body"] = ""
             return reply_header.create_response_header(config, report)
     except Exception as e:
         sys.stderr.write(f'handle_server_request: error: {e}\n')
-        report["response"]["status_code"] = "500"
-        report["response"]["body"] = "500 Internal Server Error"
-        return reply_header.create_response_header(config, report)
